42f00cb4ba3d64f495ee7f14434850b4832da39f
Zack Piper <zack@apertron.net>
2017-12-17 22:06:20 +0000

    aaaa

--- op2fvpn/AppDelegate.swift
+++ op2fvpn/AppDelegate.swift
@@ -19,22 +19,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         NSAppleEventManager.shared().setEventHandler(self, andSelector: #selector(self.handleGetURL(event:reply:)), forEventClass: UInt32(kInternetEventClass), andEventID: UInt32(kAEGetURL) )
     }
     
+    func dialogOKCancel(question: String, text: String) -> Bool {
+        let alert = NSAlert()
+        alert.messageText = question
+        alert.informativeText = text
+        alert.alertStyle = .critical
+        alert.addButton(withTitle: "OK")
+        alert.addButton(withTitle: "Cancel")
+        return alert.runModal() == .alertFirstButtonReturn
+    }
+    
+    
     @objc func handleGetURL(event: NSAppleEventDescriptor, reply:NSAppleEventDescriptor) {
         if let urlString = event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue {
             print("got urlString \(urlString)")
+            let answer = dialogOKCancel(question: "Ok?", text: "Choose your answer.")
+
             let urlCo = URLComponents(string: urlString)
             let myAppleScript = """
             tell application "iTerm2"
-            create window with default profile command "echo \(urlCo?.host ?? "invalid") && sleep 200"
+            create window with default profile
+
+                tell current window
+                    tell current session
+            write text "sudo exec openfortivpn \(urlCo?.host ?? "not supplied"):\(urlCo?.port ?? 0) -u \(urlCo?.user ?? "not supplied") -p \(urlCo?.password ?? "not supplied")"
+                    end tell
+                end tell
             end tell
             """
            
-
-            let proc = Process()
-            proc.launchPath = "/usr/bin/env"
-            proc.arguments = ["/usr/bin/osascript", "-"]
-            proc.launch()
-
+            var error: NSDictionary?
+            if let scriptObject = NSAppleScript(source: myAppleScript) {
+                if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
+                    &error) {
+                    print(output.stringValue)
+                } else if (error != nil) {
+                    print("error: \(error)")
+                }
+            }
         }
     }
     
--- op2fvpn/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "somecommandlineapp",
-    dependencies: [
-        .Package(url: "https://github.com/kareman/SwiftShell.git", majorVersion: 4)
-    ]
-)
