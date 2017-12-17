a824cca1cf7b0dd255da6c68ae7a532cc627a2da
Zack Piper <zack@apertron.net>
2017-12-17 20:45:03 +0000

    -

--- op2fvpn/AppDelegate.swift
+++ op2fvpn/AppDelegate.swift
@@ -1,46 +1,44 @@
-//
-//  AppDelegate.swift
-//  op2fvpn
-//
-//  Created by Xorkle on 13/12/2017.
-//  Copyright © 2017 Apertron. All rights reserved.
-//
-
 import Cocoa
 
-func dialogOKCancel(question: String, text: String) -> Bool {
-    let alert = NSAlert()
-    alert.messageText = question
-    alert.informativeText = text
-    alert.alertStyle = .warning
-    alert.addButton(withTitle: "OK")
-    alert.addButton(withTitle: "Cancel")
-    return alert.runModal() == .alertFirstButtonReturn
-}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
- 
-
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
     func applicationWillFinishLaunching(_ notification: Notification) {
         NSAppleEventManager.shared().setEventHandler(self, andSelector: #selector(self.handleGetURL(event:reply:)), forEventClass: UInt32(kInternetEventClass), andEventID: UInt32(kAEGetURL) )
-        dialogOKCancel(question: "a", text: "a")
     }
     
     @objc func handleGetURL(event: NSAppleEventDescriptor, reply:NSAppleEventDescriptor) {
         if let urlString = event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue {
             print("got urlString \(urlString)")
+            let urlCo = URLComponents(string: urlString)
+            let myAppleScript = """
+            tell application "iTerm2"
+            create window with default profile command "echo \(urlCo?.host ?? "invalid") && sleep 200"
+            end tell
+            """
+           
+
+            let proc = Process()
+            proc.launchPath = "/usr/bin/env"
+            proc.arguments = ["/usr/bin/osascript", "-"]
+            proc.launch()
+
         }
     }
-
     
-
-    func applicationWillTerminate(_ notification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
+    
 }
+
 
