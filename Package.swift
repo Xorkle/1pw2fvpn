23464caad0870b40038cf019848caa8f78ceb9e7
Zack Piper <zack@apertron.net>
2017-12-18 22:16:24 +0000

    ;D

---
+++ Package.swift
@@ -0,0 +1,6 @@
+import PackageDescription
+
+let package = Package(
+    name: "op2fvpn"
+  
+)
--- op2fvpn/AppDelegate.swift
+++ op2fvpn/AppDelegate.swift
@@ -25,42 +25,82 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         alert.informativeText = text
         alert.alertStyle = .critical
         alert.addButton(withTitle: "OK")
-        alert.addButton(withTitle: "Cancel")
         return alert.runModal() == .alertFirstButtonReturn
     }
-    
+
+    func activate() -> Void {
+        NSRunningApplication.current.activate(options: .activateIgnoringOtherApps)
+
+    }
     
     @objc func handleGetURL(event: NSAppleEventDescriptor, reply:NSAppleEventDescriptor) {
-        if let urlString = event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue {
-            print("got urlString \(urlString)")
-            let answer = dialogOKCancel(question: "Ok?", text: "Choose your answer.")
+        var notWell = false
+        var user, password, host, port : String
+        user = ""; password = ""; host = ""; port = ""
 
-            let urlCo = URLComponents(string: urlString)
-            let myAppleScript = """
-            tell application "iTerm2"
-            create window with default profile
+        let urlString = event.paramDescriptor(forKeyword: keyDirectObject)?.stringValue
 
-                tell current window
-                    tell current session
-            write text "sudo exec openfortivpn \(urlCo?.host ?? "not supplied"):\(urlCo?.port ?? 0) -u \(urlCo?.user ?? "not supplied") -p \(urlCo?.password ?? "not supplied")"
-                    end tell
-                end tell
-            end tell
-            """
-           
-            var error: NSDictionary?
-            if let scriptObject = NSAppleScript(source: myAppleScript) {
-                if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
-                    &error) {
-                    print(output.stringValue)
-                } else if (error != nil) {
-                    print("error: \(error)")
-                }
+        
+        let urlCo = URLComponents(string: urlString!)
+        
+        if let user2: String = urlCo?.user {
+            user =  user2
+        } else {
+            notWell = true
+            activate()
+            dialogOKCancel(question: "A username is required", text: "Please supply a username parameter")
+        }
+
+        if let port2: Int = urlCo?.port {
+            port =  String(port2)
+        } else {
+            notWell = true
+            activate()
+            dialogOKCancel(question: "A port is required", text: "Please supply a port parameter")
+        }
+       
+        if let password2: String = urlCo?.password {
+            password = password2
+        } else {
+            notWell = true
+            activate()
+            dialogOKCancel(question: "A password is required", text: "Please supply a password parameter")
+        }
+        
+        if let host2: String = urlCo?.host {
+            host =  host2
+        } else {
+            notWell = true
+            activate()
+            dialogOKCancel(question: "A hostname is required", text: "Please supply a hostname parameter")
+        }
+
+        if (notWell) {
+          NSApp.terminate(self)
+        }
+
+        let myAppleScript = """
+          tell application "iTerm2"
+          create window with default profile
+
+          tell current window
+          tell current session
+          write text "sudo openfortivpn \(host):\(port) -u \(user) -p \(password)"
+          end tell
+          end tell
+          end tell
+          """
+        print(myAppleScript)
+        var error: NSDictionary?
+        if let scriptObject = NSAppleScript(source: myAppleScript) {
+            if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
+                 &error) {
+                print(output.stringValue)
+            } else if (error != nil) {
+                print("error: \(error)")
             }
         }
     }
-    
-    
 }
 
 
